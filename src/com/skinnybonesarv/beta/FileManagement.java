d4fe1e80b13610256c6ba0fe3e955b31fac5835a
SkinnyBonesArv <SkinnyBonesArv@Gmail.com>
2018-04-11 09:45:02 -0400

    Updated packages

---
+++ src/com/skinnybonesarv/beta/FileManagement.java
@@ -0,0 +1,13 @@
+package com.skinnybonesarv.beta;
+
+import java.util.List;
+
+public class FileManagement {
+
+    public static void addToFile(List<Student> students) {
+        addToFile(students);
+        // Add contents of List to file
+
+    }
+
+}
---
+++ src/com/skinnybonesarv/beta/Main.java
@@ -0,0 +1,11 @@
+package com.skinnybonesarv.beta;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to SkinnyBonesArv's School Management System!");
+        // Welcome Screen
+        do {
+            // Do something
+        } while (true);
+    }
+}
