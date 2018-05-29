99ead14c5f8dcc584077c3a58507544a3e5584aa
SkinnyBonesArv <SkinnyBonesArv@Gmail.com>
2018-05-29 10:52:49 -0400

    Rewrite of project is complete for now

--- src/com/skinnybonesarv/Main.java
+++ src/com/skinnybonesarv/Main.java
@@ -1,114 +1,181 @@
-/**
- * @author SkinnyBonesArv
- * @version 1/15/2018
- */
-
 package com.skinnybonesarv;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 /**
- * TODO:
- * - Finish ability to modify students
- * - Modify code for removing students to be cleaner and more efficient
- * - Add the ability for teachers to managed
- * - Add abstract class/interface for human object so that teacher and student objects can have a parent object
- * - Move student management functionality from FileIO class
- * - Organize program in more packages
- *
- * Possible Changes:
- * - Use SQL database or JSON file instead of .txt file
- * - Add ability for user to manage classes/schedule for a student
- * - Make program GUI-based through JavaFX or Android
- * - Use a framework to modularize the code even more (like MVC)
+ * @author SkinnyBonesArv
  */
-
 public class Main {
 
+    private static List<Student> sStudentList = null;
+    private static Scanner sConsoleIn = new Scanner(System.in);
+
     public static void main(String[] args) {
-        /*try {
-            // ----------- DECLARATIONS -----------
-            FileIO fileIO = new FileIO();
-            File studentInfoFile = fileIO.createFile("student_info.txt");
+        int option = 0;
 
-            ArrayList<Student> studentList;
+        System.out.println("-------------------------------------------------------------------------------------");
+        System.out.println("Welcome to SkinnyBonesArv's School Management System!");
+        System.out.println("Version: Alpha 1.0");
+        System.out.println("-------------------------------------------------------------------------------------");
 
-            int choice;
-            int number;
+        if (Student.getFileLength() > 0) {
+            sStudentList = Student.readFromFile();
+        } else {
+            System.out.println("Due to this being the first run of the program, you must create one student.");
+            addStudent();
+        }
 
-            Scanner in = new Scanner(System.in);
-
-            // ----------- WELCOME SCREEN -----------
-            System.out.println("-------------------------------------------------------------------------------------");
-            System.out.println("Welcome to the School Management System!");
-            System.out.println("Version: Alpha 2.0");
-            System.out.println("Copyright © 2017 SkinnyBonesArv.  All rights reserved.");
-            System.out.println("-------------------------------------------------------------------------------------");
-
-            // ----------- MAIN SCREEN -----------
-            if (fileIO.getFileLength(studentInfoFile) == 0) {
-                while (true) {
-                    System.out.println();
-                    System.out.print("Would you like to add students (1), remove students (2), " +
-                            "or modify students (3)? ");
-                    choice = in.nextInt();
-
-                    switch (choice) {
-                        case 1:
-                            // Input number of students to add
-                            System.out.print("How many students would you like to add?  ");
-                            number = in.nextInt();
-
-                            studentList = FileIO.createStudent(studentInfoFile, number, in);
-                            FileIO.addStudent(studentInfoFile, studentList);
-                            break;
-                        case 2:
-                            // Display students
-                            System.out.println("First: Last: Sex: Grade: Date of Birth: ID:");
-                            studentList = FileIO.viewStudents(studentInfoFile);
-
-                            // Input ID of the student to delete
-                            System.out.print("Enter the ID of the student you wish to remove: ");
-                            choice = in.nextInt();
-
-                            FileIO.deleteStudent(studentInfoFile, studentList, choice);
-
-                            studentList = FileIO.viewStudents(studentInfoFile);
-                            break;
-                        case 3:
-                            // Display students
-                            System.out.println("First: Last: Sex: Grade: Date of Birth: ID:");
-                            studentList = FileIO.viewStudents(studentInfoFile);
-
-                            // Input ID of the student to modify
-                            System.out.print("Enter the ID of the student you wish to remove: ");
-                            choice = in.nextInt();
-
-                            FileIO.modifyStudent(studentInfoFile, studentList, choice);
-
-                            studentList = FileIO.viewStudents(studentInfoFile);
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                // ----------- FIRST TIME USE SCREEN -----------
-            } else {
+        while (option != 5) {
+            System.out.println("\nWould you like to:\n" +
+                    "\t- Add a student (1)\n" +
+                    "\t- Remove a student (2)\n" +
+                    "\t- Modify a student (3)\n" +
+                    "\t- View all students (4)\n" +
+                    "\t- Exit the program (5)");
+            try {
+                option = sConsoleIn.nextInt();
                 System.out.println();
-                System.out.println("Welcome to the student setup configuration!");
 
-                // Input number of students to add
-                System.out.print("How many students would you like to add? (You can add more later.) ");
-                number = in.nextInt();
-
-                studentList = FileIO.createStudent(studentInfoFile, number, in);
-                FileIO.addStudent(studentInfoFile, studentList);
+                switch (option) {
+                    case 1:
+                        addStudent();
+                        break;
+                    case 2:
+                        removeStudent();
+                        break;
+                    case 3:
+                        modifyStudent();
+                        break;
+                    case 4:
+                        viewAllStudents();
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        System.out.println("Invalid option.");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input type.");
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
+        }
     }
+
+    private static void addStudent() {
+        System.out.print("Enter last name: ");
+        String lastName = sConsoleIn.next();
+
+        System.out.print("Enter first name: ");
+        String firstName = sConsoleIn.next();
+
+        System.out.print("Enter sex (Male, Female, or Other): ");
+        String sexStr = sConsoleIn.next();
+
+        System.out.print("Enter year of birth: ");
+        int yearBorn = sConsoleIn.nextInt();
+        System.out.print("Enter month of birth: ");
+        int monthBorn = sConsoleIn.nextInt();
+        System.out.print("Enter day of birth: ");
+        int dayBorn = sConsoleIn.nextInt();
+
+        System.out.print("Enter grade: ");
+        int grade = sConsoleIn.nextInt();
+
+        sStudentList.add(new Student(lastName, firstName, LocalDate.of(yearBorn, monthBorn, dayBorn),
+                Person.Sex.valueOf(sexStr.toUpperCase()), grade));
+        sStudentList.get(sStudentList.size() - 1).writeToFile();
+
+        System.out.println("Operation completed successfully.");
+    }
+
+    private static void removeStudent() {
+        viewAllStudents();
+        int id = 0;
+        System.out.print("\nEnter the ID # of the student you'd like to remove: ");
+        try {
+            id = sConsoleIn.nextInt();
+
+            sStudentList.remove(id - 1);
+            Student.saveChangesToFile(sStudentList);
+            System.out.println("Operation completed successfully.");
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input type.");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Invalid id number.");
+        }
+    }
+
+    private static void modifyStudent() {
+    	viewAllStudents();
+    	int id = 0;
+    	int choice = 0;
+        System.out.print("\nEnter the ID # of the student you'd like to modify: ");
+        try {
+            id = sConsoleIn.nextInt();
+
+            System.out.println("Which part of the student would you like to modify?" +
+                    "\t- Last Name (1)\n" +
+                    "\t- First Name (2)\n" +
+                    "\t- Sex (3)\n" +
+                    "\t- Date of Birth (4)\n" +
+                    "\t- Grade (5)");
+            choice = sConsoleIn.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter new last name: ");
+                    String lastName = sConsoleIn.next();
+                    sStudentList.get(id - 1).setLastName(lastName);
+                    break;
+                case 2:
+                    System.out.print("Enter new first name: ");
+                    String firstName = sConsoleIn.next();
+                    sStudentList.get(id - 1).setFirstName(firstName);
+                    break;
+                case 3:
+                    System.out.print("Enter sex (Male, Female, or Other): ");
+                    String sexStr = sConsoleIn.next();
+                    sStudentList.get(id - 1).setSex(Person.Sex.valueOf(sexStr.toUpperCase()));
+                    break;
+                case 4:
+                    System.out.print("Enter year of birth: ");
+                    int yearBorn = sConsoleIn.nextInt();
+                    System.out.print("Enter month of birth: ");
+                    int monthBorn = sConsoleIn.nextInt();
+                    System.out.print("Enter day of birth: ");
+                    int dayBorn = sConsoleIn.nextInt();
+                    sStudentList.get(id - 1).setDateOfBirth(LocalDate.of(yearBorn, monthBorn, dayBorn));
+                    break;
+                case 5:
+                    System.out.print("Enter grade: ");
+                    int grade = sConsoleIn.nextInt();
+                    sStudentList.get(id - 1).setGrade(grade);
+                    break;
+            }
+
+            Student.saveChangesToFile(sStudentList);
+            System.out.println("Operation completed successfully.");
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input type.");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Invalid id number.");
+        }
+    }
+
+    private static void viewAllStudents() {
+        System.out.printf("%-15s %-15s %-20s %-10s %-10s %s%n", "Last Name:", "First Name:", "Date of Birth:", "Sex:",
+                "Grade:", "ID #:");
+        // Sort list alphabetically based on last name
+        Collections.sort(sStudentList, new PersonLastNameComparator());
+        for (Student s : sStudentList)
+            System.out.println(s);
+
+        // Sort list by id for other operations (mainly for saving changes to the file)
+        Collections.sort(sStudentList, new PersonIDComparator());
+    }
+
 }
---
+++ src/com/skinnybonesarv/Schedule.java
@@ -0,0 +1,57 @@
+package com.skinnybonesarv;
+
+import java.util.Arrays;
+
+/**
+ * TODO: Finish implementing toString method.
+ * WARNING: Not fully developed yet.
+ *
+ * @author SkinnyBonesArv
+ */
+public class Schedule {
+
+    private int mNumOfCourses;
+    private Course[] mCourses;
+
+    public Schedule(int numOfCourses) {
+        mCourses = new Course[numOfCourses];
+        mNumOfCourses = numOfCourses;
+    }
+
+    public void addCourse(Course course, int period) {
+        try {
+            mCourses[period] = course;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Already enough courses in schedule.");
+            e.printStackTrace();
+        }
+    }
+
+    public Course getCourse(int period) {
+        return mCourses[period];
+    }
+
+    public int getPeriod(Course course) {
+        return Arrays.asList(mCourses).indexOf(course); // Doesn't work
+    }
+
+    public void setPeriod(Course course, int period) {
+        if (mCourses[period] != null) {
+            int oldPeriod = Arrays.asList(course).indexOf(course); // Might not work
+            Course oldCourse = mCourses[period];
+            mCourses[period] = course;
+            mCourses[oldPeriod] = oldCourse;
+        } else if (Arrays.asList(mCourses).contains(course)) {
+            Arrays.asList(mCourses).remove(course);
+            mCourses[period] = course;
+        } else {
+            mCourses[period] = course;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return new String().format("Schedule - Number of Courses: %d", mNumOfCourses);
+    }
+
+}
